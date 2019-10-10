b6270b79b45103088de8e4eed3687e44d7bbd835
github username <github email address>
2019-10-10 14:27:30 +0100

    Done.

--- events.js
+++ events.js
@@ -6,11 +6,31 @@
 // };
 
 //1
-document.addEventListener("click", function(){
+document.querySelector("#click").addEventListener("click", function(){
     document.querySelector("#click").innerHTML = "You clicked me";
 });
 
 //2
-// document.addEventListener("ondblclick", function(){
-//     alert('I was clicked!');
-// });
+document.querySelector("#dblclick").addEventListener("dblclick", function(){
+    alert('I was clicked!');
+});
+
+function find(array, criteriaFn) {
+   
+    let current = array
+    let next = []
+
+    while (current || current === 0) {
+     if (criteriaFn(current)) {
+        return current
+      }
+   
+     if (Array.isArray(current)) {
+        for (let i = 0; i < current.length; i++) {
+          next.push(current[i])
+        }
+      }
+      current = next.shift()
+    }
+    return null
+  }
