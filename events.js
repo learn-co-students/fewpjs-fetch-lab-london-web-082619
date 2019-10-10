9b972c5cf11f1791cecbf67bf5f877f6974f51f3
github username <github email address>
2019-10-10 09:16:20 +0100

    Done.

--- events.js
+++ events.js
@@ -1,15 +1,16 @@
 
-// document.addEventListener("click", myClick); //pattern1 eventlistner and fucntion are declared separately
+// document.addEventListener("click", myClick); //this works the same as //1.  
 
 // function myClick(){
 //     document.querySelector("#click").innerHTML = "You clicked me";
 // };
 
-
+//1
 document.addEventListener("click", function(){
     document.querySelector("#click").innerHTML = "You clicked me";
 });
 
+//2
 // document.addEventListener("ondblclick", function(){
 //     alert('I was clicked!');
 // });
